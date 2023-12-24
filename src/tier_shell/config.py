a453ddd5af6e8ec9ca5e7ca20d6083a60ccde0a8
khainguyen <khainguyen@umass.edu>
2023-12-24 01:55:37 +0000

    [add] app config and base config class

--- src/tier_shell/config.py
+++ src/tier_shell/config.py
@@ -1,4 +1,7 @@
-from typing import Optional
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Any, Callable, Dict, Optional
 
 import os
 
@@ -7,31 +10,80 @@ from pathlib import Path
 from yarl import URL
 
 
-class ApiConfig:
-    """Manage API service environment variables"""
-    def __init__(self, path: Optional[str | Path] = None):
+@dataclass(init=True)
+class _AttributeData:
+    default_value: Any
+    typecast: Callable[[Any], Any] = str
+
+
+def _private_attr_name(name: str) -> str:
+    return '_' + name
+
+
+class BaseConfig(ABC):
+    _ENV_VARS: Dict[str, _AttributeData] = {}
+    
+    def __init__(self):
+        # Set default private attributes
+        for attr, data in self._ENV_VARS.items():
+            setattr(self, _private_attr_name(attr), data.default_value)
+    
+    @abstractmethod
+    def from_env(self, path: str | URL):
         if path is None:
             load_dotenv()
-        elif not os.path.isabs(path):
-            path = Path(__file__).parent.resolve() / Path(path)
-            load_dotenv(path)
         else:
             load_dotenv(path)
             
-        self._PORT: int = int(os.getenv('PORT', default=-1))
-        self._TIMEOUT_SECONDS: float = float(os.getenv('TIMEOUT_SECONDS', default=8))
-        self._BASE_URL: URL = URL(os.getenv('BASE_URL', default='http://localhost'))
-        self._API_PATH: str = os.getenv('API_PATH', default='')
-        self._API_ROOT_URL = self._BASE_URL / self._API_PATH
+        for attr, data in self._ENV_VARS.items():
+            if attr in os.environ:
+                setattr(
+                    self, 
+                    _private_attr_name(attr), 
+                    data.typecast(os.getenv(attr))
+                    )
+
+
+class ApiConfig(BaseConfig):
+    """Manage API service environment variables."""
+    
+    _ENV_VARS = {
+        'PORT': _AttributeData(
+            default_value=None, 
+            typecast=int
+            ),
+        'TIMEOUT_SECONDS': _AttributeData(
+            default_value=float('inf'), 
+            typecast=float
+            ),
+        'BASE_URL': _AttributeData(
+            default_value=URL('http://localhost'), 
+            typecast=URL
+            ),
+        'API_PATH': _AttributeData(
+            default_value='',
+            typecast=str
+            ),
+    }
+    
+    def __init__(self):
+        super().__init__()
         
-        # Normalize service URL
+    def from_env(self, path: str | URL) -> ApiConfig:
+        super().from_env(path)
+    
+        # Construct API root URL
+        self._API_ROOT_URL = self._BASE_URL / self._API_PATH
+
         # If port was provided then append to URL
-        # If no scheme was provided then set default to http
         if self._PORT != -1:
             self._API_ROOT_URL = self._API_ROOT_URL.with_port(self._PORT)
         
+        # If no scheme was provided then set default to http
         if not self._API_ROOT_URL.scheme:
             self._API_ROOT_URL = self._API_ROOT_URL.with_scheme("http")
+            
+        return self
     
     @property
     def port(self):
@@ -52,4 +104,17 @@ class ApiConfig:
     @property
     def api_root_url(self):
         return self._API_ROOT_URL
+    
+    
+class AppConfig(BaseConfig):
+    def __init__(self):
+        self._api = ApiConfig()
+        
+    def from_env(self, path: str | URL) -> AppConfig:
+        self._api = self._api.from_env(path)
+        return self
+        
+    @property
+    def api(self):
+        return self._api
     
