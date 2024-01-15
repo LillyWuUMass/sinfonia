2e9aa7a367ceadd20421153b847bc95f2451bf8c
khainguyen <khainguyen@umass.edu>
2024-01-15 10:49:59 +0000

    [add] tier shell env example

--- src/sinfonia/app_tier1.py
+++ src/sinfonia/app_tier1.py
@@ -125,7 +125,7 @@ def wsgi_app_factory(**args) -> connexion.FlaskApp:
     # handle running behind reverse proxy (should this be made configurable?)
     flask_app.wsgi_app = ProxyFix(flask_app.wsgi_app)
 
-    # add Tier1 APIs
+    # Add Tier1 APIs
     app.add_api(
         load_spec(app.specification_dir / "sinfonia_tier1.yaml"),
         resolver=MethodViewResolver("sinfonia.api_tier1"),
